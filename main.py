6dfa92536e36682a40dc4a8e9494ac3c6537f536
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-06-26 18:30:40 +0800

    my first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+USERNAME = ""
+PASSWORD = ""
+
+
+class Instagram:
+    def __init__(self, username, password):
+        self.driver = webdriver.Chrome("./chromedriver.exe")
+        self.username = username
+        self.password = password
+
+    def signin(self):
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        inputs = self.driver.find_elements_by_css_selector('form input')
+
+        emailInput = inputs[0]
+        passwordInput = inputs[1]
+        emailInput.send_keys(self.username)
+
+        passwordInput.send_keys(self.password)
+        passwordInput.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        current_url = self.driver.current_url
+        self.is_signin = current_url == "https://www.instagram.com/"
+
+    def go_to_user(self, user):
+        if (not self.is_signin):
+            print("User not signed in")
+            return
+        self.driver.get("https://www.instagram.com/" + user)
+
+
+if __name__ == "__main__":
+    instagram = Instagram(USERNAME, PASSWORD)
+    instagram.signin()
+    instagram.go_to_user("melvinoyx")
